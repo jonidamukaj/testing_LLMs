47bda86f00579b3cdfb79a4d6cfd254561dda053
Ida <jonida.mukaj@stud.uni-bamberg.de>
2023-07-27 10:29:12 +0200

    last changes 2

--- testing/paragraph1/prompt1.py
+++ testing/paragraph1/prompt1.py
@@ -10,7 +10,7 @@ openai.api_key = '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'
 prompt_template1 = "Please describe briefly the following scientific author and consider the following information:\n\nName: {name}\nPublications: {publications}\nJournal Articles: {journal_articles}\nsince year:{since_year}\nProceedings Papers: {proceedings_papers}\n\n"
 prompt_template2 = "Please describe briefly with a few words the following scientific author and consider the following information:\n\nName: {name}\nPublications: {publications}\nJournal Articles: {journal_articles}\nsince year:{since_year}\nProceedings Papers: {proceedings_papers}\n\n"
 file_path = os.path.abspath("testing/data_authors.json")
-output_file = os.path.abspath("testing/prompt1_results.json")
+output_file = os.path.abspath("testing/prompt11_results.json")
 
 
 def save_to_json(data):
