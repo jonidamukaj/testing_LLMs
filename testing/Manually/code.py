39392e7a2e3fe3ca1cf42397d86d6881eca8ad63
Ida <jonida.mukaj@stud.uni-bamberg.de>
2023-09-18 00:51:36 +0200

    drf

--- testing/Manually/code.py
+++ testing/Manually/code.py
@@ -1,25 +1,56 @@
-import csv
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Define the data in the same format as provided
-data = [
-    ["Paragraph", "Model", "Coherence", "Consistency", "Relevance", "Fluency", "comment"],
-    ["Paragraph1", "ada", 1.783333333, 1.383333333, 1.366666667, 3, ""],
-    ["Paragraph1", "curie", 2.533333333, 1.783333333, 1.416666667, 3.616666667, ""],
-    ["Paragraph1", "davinci", 4.016666667, 3.733333333, 3.9, 4.033333333, ""],
-    ["Paragraph1", "turbo", 4.866666667, 4, 4.266666667, 4.916666667, ""],
-    ["Paragraph2", "ada", 2.633333333, 2.616666667, 2.7, 2.816666667, "hallucinated text, they instead of he/she"],
-    ["Paragraph2", "curie", 3.633333333, 3.6, 3.616666667, 4.133333333, ""],
-    ["Paragraph2", "davinci", 4.083333333, 3.783333333, 3.95, 3.533333333, ""],
-    ["Paragraph2", "turbo", 4.966666667, 4.416666667, 4.95, 4.9, ""],
-    ["Paragraph3", "ada", 0, 0, 0, 0, ""],
-    ["Paragraph3", "curie", 2.75, 2.416666667, 2.633333333, 2.716666667, "hallucinated text, biologist, physicist etc"],
-    ["Paragraph3", "davinci", 3.25, 3.3, 3.316666667, 3.033333333, ""],
-    ["Paragraph3", "turbo", 4.166666667, 3.916666667, 4.383333333, 4.3, ""]
-]
+# Load your data into a pandas DataFrame
+data = pd.read_csv('automatic.csv')
 
-# Save the data to a CSV file
-with open('manually.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerows(data)
+# Calculate correlations with Coherence
+coherence_correlation = data[['F1_Score', 'Rouge1', 'Rouge2', 'RougeL', 'Length', 'Vec2', 'Bleu']].corrwith(data['Coherence'], method='pearson')
 
-print("Data saved to output.csv")
+# Calculate correlations with Consistency
+consistency_correlation = data[['F1_Score', 'Rouge1', 'Rouge2', 'RougeL', 'Length', 'Vec2', 'Bleu']].corrwith(data['Consistency'], method='pearson')
+
+# Calculate correlations with Relevance
+relevance_correlation = data[['F1_Score', 'Rouge1', 'Rouge2', 'RougeL', 'Length', 'Vec2', 'Bleu']].corrwith(data['Relevance'], method='pearson')
+
+# Calculate correlations with Fluency
+fluency_correlation = data[['F1_Score', 'Rouge1', 'Rouge2', 'RougeL', 'Length', 'Vec2', 'Bleu']].corrwith(data['Fluency'], method='pearson')
+
+# Visualize the correlations for each human metric
+plt.figure(figsize=(12, 6))
+
+# Bar plot for Coherence
+plt.subplot(221)
+coherence_correlation.plot(kind='bar', color='skyblue')
+plt.xlabel('Automatic Metrics')
+plt.ylabel('Correlation with Coherence')
+plt.title('Correlation between Automatic Metrics and Coherence')
+plt.xticks(rotation=45)
+
+# Bar plot for Consistency
+plt.subplot(222)
+consistency_correlation.plot(kind='bar', color='lightgreen')
+plt.xlabel('Automatic Metrics')
+plt.ylabel('Correlation with Consistency')
+plt.title('Correlation between Automatic Metrics and Consistency')
+plt.xticks(rotation=45)
+
+# Bar plot for Relevance
+plt.subplot(223)
+relevance_correlation.plot(kind='bar', color='lightcoral')
+plt.xlabel('Automatic Metrics')
+plt.ylabel('Correlation with Relevance')
+plt.title('Correlation between Automatic Metrics and Relevance')
+plt.xticks(rotation=45)
+
+# Bar plot for Fluency
+plt.subplot(224)
+fluency_correlation.plot(kind='bar', color='lightsalmon')
+plt.xlabel('Automatic Metrics')
+plt.ylabel('Correlation with Fluency')
+plt.title('Correlation between Automatic Metrics and Fluency')
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
+
